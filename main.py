848e85450510984f1878bd029429bec999f06a53
Vladimir Irkhin <ivb1989@yandex.ru>
2023-01-21 21:50:42 +0300

    Feature : create alter data for new xml file.

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 from collections import defaultdict
 from googletrans import Translator
-
-files = 'upload/data.txt'
+from config import header, footer, files
+from pprint import pprint
 
 
 def get_tables_and_columns(data) -> defaultdict:
+    """
+    Получаем из файла структуру по принципу:
+    Таблица : Список колонок (на русском или текущем языке)
+
+    :return defaultdict(list)
+    """
     hash_tables = defaultdict(list)
 
     with open(data, 'r', encoding='UTF-8') as f:
@@ -21,6 +27,12 @@ def get_tables_and_columns(data) -> defaultdict:
 
 
 def translate_columns() -> defaultdict:
+    """
+    Перевод колонок и на выходе получаем структуру:
+    Таблица: список колонок на английском языке
+
+    :return: defaultdict(list)
+    """
     current_structure_data = get_tables_and_columns(files)
     translator = Translator()
 
@@ -31,3 +43,28 @@ def translate_columns() -> defaultdict:
     return current_structure_data
 
 
+def get_data_alter_name_columns():
+    """
+    Мэппинг Alter column для текущего текста колонок и колонок на английском.
+    Содержимое в дальнейшем используется для создания итогового xml файла.
+
+    :in1: Таблица : Список колонок (на русском или текущем языке)
+    :in2:  Таблица: список колонок на английском языке
+    :return: defaultdict(list)
+    """
+    old_name_structure = get_tables_and_columns(files)
+    new_name_structure = translate_columns()
+    alter_name_data = defaultdict(list)
+
+    for table1, column1 in old_name_structure.items():
+        for table2, column2 in new_name_structure.items():
+            list_all_columns = list(zip(column1, column2))
+            for elem in list_all_columns:
+                old, new = elem
+                data = f'ALTER TABLE {table1} RENAME COLUMN "{old}" to {new}'
+                alter_name_data[table1].append(data)
+
+    return alter_name_data
+
+
+pprint(get_data_alter_name_columns())
