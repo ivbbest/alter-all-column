c916423d333e41abcc80fcd0d3e3214d9d470586
Vladimir Irkhin <ivb1989@yandex.ru>
2023-01-22 15:50:08 +0300

    Удалил отдельную функцию, в которой создавались строки для комментариев в xml. Изменил логику создания xml файла.

--- main.py
+++ main.py
@@ -46,13 +46,12 @@ def translate_columns() -> defaultdict:
     return current_structure_data
 
 
-def merged_translations_columns():
+def merged_columns_for_tables(*tables):
     """Объединение по таблицам колонок с текущим названием и переведенным"""
-    current_name_columns = get_tables_and_columns(input_file)
-    translate_name_columns = translate_columns()
+
     merged_elements = defaultdict(list)
 
-    for common_table in (current_name_columns, translate_name_columns):
+    for common_table in tables:
         for key, value in common_table.items():
             merged_elements[key].append(tuple(value))
 
@@ -64,8 +63,12 @@ def get_alter_and_comment_data():
     """
     Получаем готовые строки с alter и comment для xml файла
     """
-    tables_and_columns = merged_translations_columns()
+    current_name_columns = get_tables_and_columns(input_file)
+    translate_name_columns = translate_columns()
+
+    tables_and_columns = merged_columns_for_tables(current_name_columns, translate_name_columns)
     tables = tables_and_columns.keys()
+
     alter_data = defaultdict(list)
     comment_data = defaultdict(list)
 
@@ -80,46 +83,23 @@ def get_alter_and_comment_data():
     return alter_data, comment_data
 
 
-# TODO: исправить на DRY c предыдущей функцией get_data_alter_name_columns()
-# TODO: неправильно добавляется связка новый - старый комментарий. Неверный порядок вписывается.
-def get_comment_for_column():
-    """
-    Получаем комментарии на русском языке для каждого столбца
-    :return:
-    """
-    old_name_structure = get_tables_and_columns(input_file)
-    new_name_structure = translate_columns()
-    comment_name_data = defaultdict(list)
-    breakpoint()
+def create_xml_file():
+    """Создание xml файла"""
+    alter_columns_text, comment_columns_text = get_alter_and_comment_data()
+    merged_dataset = merged_columns_for_tables(alter_columns_text, comment_columns_text)
+    i = 1
+    tables = merged_dataset.keys()
 
-    for table1, column1 in old_name_structure.items():
-        for table2, column2 in new_name_structure.items():
-            list_all_columns = list(zip(column1, column2))
-            for elem in list_all_columns:
-                old, new = elem
-                data = f'comment on column {table1}.{new} is "{old}";'
-                comment_name_data[table1].append(data)
+    for table in tables:
+        alter_rows, comment_rows = merged_dataset[table]
+        name_file = f"{datetime.now().strftime('%Y-%m-%d')}-{i}-alter_table_{table}.xml"
+        alter = '\n'.join(alter_rows)
+        comment = '\n'.join(comment_rows)
+        with open(f'{output_dir}/{name_file}', 'w', encoding='UTF-8') as f:
+            info = f'{header}\n{alter}\ncommit;\n\n{comment}\ncommit;\n\n{footer}'
+            f.write(info)
 
-    return comment_name_data
+        i += 1
 
 
-# TODO: некорректно добавляется информация. 3 раза создается один и тот же файл, а также 3 раза добавляется разделы alter и comments
-# def create_xml_file():
-#     alter_columns_text = get_data_alter_name_columns()
-#     comment_columns_text = get_comment_for_column()
-#     i = 1
-#
-#     for table1, column1 in alter_columns_text.items():
-#         for table2, column2 in comment_columns_text.items():
-#             name_file = f"{datetime.now()}-{i}-alter_table_{table2}.xml"
-#             alter = '\n'.join(column1)
-#             comment = '\n'.join(column2)
-#             with open(f'{output_dir}/{name_file}', 'w', encoding='UTF-8') as f:
-#                 info = f'{header}\n{alter}\ncommit;\n{comment}\ncommit;\n{footer}'
-#                 f.write(info)
-#
-#             i += 1
-
-
-# create_xml_file()
-pprint(get_alter_and_comment_data())
+create_xml_file()
