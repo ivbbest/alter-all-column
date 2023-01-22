26100f47792d29cbd20b8b5ecbe1f3ce42b9dba7
Vladimir Irkhin <ivb1989@yandex.ru>
2023-01-22 16:24:29 +0300

    Modify config file.

--- config.py
+++ config.py
@@ -1,24 +1,10 @@
 input_file = 'upload/data.txt'
 output_dir = 'upload/xml'
-header = '''<?xml version="1.0" encoding="UTF-8"?>
+header1 = '''<?xml version="1.0" encoding="UTF-8"?>\n<databaseChangeLog\n
+        xmlns="http://www.liquibase.org/xml/ns/dbchangelog"\n
+        xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"\n
+        xsi:schemaLocation="http://www.liquibase.org/xml/ns/dbchangelog http://www.liquibase.org/xml/ns/dbchangelog/dbchangelog-3.9.xsd">\n
+    <changeSet id="alter table'''
+header2 = 'author="Irkhin-VB" dbms="oracle">\n<sql>'
 
-<databaseChangeLog
-
-        xmlns="http://www.liquibase.org/xml/ns/dbchangelog"
-
-        xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-
-        xsi:schemaLocation="http://www.liquibase.org/xml/ns/dbchangelog http://www.liquibase.org/xml/ns/dbchangelog/dbchangelog-3.9.xsd">
-
-    <changeSet id="alter table {table_name}" author="Irkhin-VB" dbms="oracle">
-
-        <sql>
-'''
-
-footer = '''</sql>
-
-                                               </changeSet>
-
-                                               </databaseChangeLog>
-
-                               '''
+footer = '</sql>\n</changeSet>\n</databaseChangeLog>'
