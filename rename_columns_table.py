6344a4e7f1b2c4691f999bdb8c2ea1913d656b23
Vladimir Irkhin <ivb1989@yandex.ru>
2023-03-11 14:00:34 +0300

    Add rollback info in xml file. And use pre-commit for py code.

--- rename_columns_table.py
+++ rename_columns_table.py
@@ -1,8 +1,18 @@
+import re
 from collections import defaultdict
-from googletrans import Translator
-from settings import header1, header2, footer, input_file, output_dir
 from datetime import datetime
 from string import ascii_letters
+
+from googletrans import Translator
+
+from settings import (
+    header1,
+    header2,
+    input_file,
+    output_dir,
+    rollback_end,
+    rollback_start,
+)
 
 translator = Translator()
 
@@ -16,12 +26,12 @@ def get_tables_and_columns(file) -> dict:
     """
     hash_tables = defaultdict(list)
 
-    with open(file, 'r', encoding='UTF-8') as f:
+    with open(file, encoding="UTF-8") as f:
         for line in f:
             line = line.strip()
-            if line.startswith('*'):
-                table = line.split('*')[1]
-            elif line == '' or is_english_word(line):
+            if line.startswith("*"):
+                table = line.split("*")[1]
+            elif line == "" or is_english_word(line):
                 continue
             else:
                 hash_tables[table].append(line)
@@ -36,12 +46,16 @@ def is_english_word(word: str) -> bool:
 
 def translated_word(text: str | list[str]) -> str | list[str] | None:
     """Перевод отдельного текста или слова"""
-    return translator.translate(text, dest='en').text
+    translated_text = translator.translate(text, dest="en").text
+
+    return re.sub(r"\s", "_", translated_text)
 
 
-# TODO: Исключить выбор колонок с английским текстом. Если колонка на английском тексте,
+# TODO: Исключить выбор колонок с английским текстом.
+#  Если колонка на английском тексте,
 #  то не переводить его еще раз
-# TODO: Создать словарь для кэширования данных по переводу. Если слово уже есть в словаре,
+# TODO: Создать словарь для кэширования данных по переводу.
+#  Если слово уже есть в словаре,
 #  то дополнительно не нужно его переводить, а брать данные из словаря
 def translated_all_columns(tables_and_columns) -> dict:
     """
@@ -50,25 +64,20 @@ def translated_all_columns(tables_and_columns) -> dict:
 
     :return: defaultdict(list)
     """
+    translated_data = defaultdict(list)
 
     for table, columns in tables_and_columns.items():
         columns = list(map(translated_word, columns))
 
-        # подумать как исправить код, чтобы работала мнемизация
-        # columns = list(
-        #     map(lambda txt, dictionary_words={'ТБ': 'TB'}: dictionary_words.setdefault(txt, translated_word(txt)),
-        #         columns))
+        translated_data[table] = columns
 
-        tables_and_columns[table] = columns
-
-    return tables_and_columns
+    return translated_data
 
 
 def merged_columns_for_tables(*tables) -> dict:
     """Объединение по таблицам колонок с текущим названием и переведенным"""
 
     merged_elements = defaultdict(list)
-
     for common_table in tables:
         for key, value in common_table.items():
             merged_elements[key].append(tuple(value))
@@ -76,49 +85,66 @@ def merged_columns_for_tables(*tables) -> dict:
     return merged_elements
 
 
-# TODO: исправить на DRY c предыдущей функцией create_xml_file() в части обработки двух списков/кортежей.
+# TODO: исправить на DRY c предыдущей функцией create_xml_file()
+#  в части обработки двух списков/кортежей.
 #  Начиная с for table in tables: и до формирования конкретных данных
 def get_alter_and_comment_data(file):
     """
-    Получаем готовые строки с alter и comment для xml файла из входящего текстового файла
+    Получаем готовые строки с alter и comment для
+    xml файла из входящего текстового файла
     """
     current_name_columns = get_tables_and_columns(file)
     translate_name_columns = translated_all_columns(current_name_columns)
 
-    tables_and_columns = merged_columns_for_tables(current_name_columns, translate_name_columns)
+    tables_and_columns = merged_columns_for_tables(
+        current_name_columns, translate_name_columns
+    )
     tables = tables_and_columns.keys()
 
     alter_data = defaultdict(list)
     comment_data = defaultdict(list)
+    rollback_data = defaultdict(list)
 
     for table in tables:
         old_name_columns, new_name_columns = tables_and_columns[table]
         for old, new in zip(old_name_columns, new_name_columns):
             alter = f'ALTER TABLE {table} RENAME COLUMN "{old}" to {new};'
-            comment = f'comment on column {table}.{old} is "{new}";'
+            comment = f'comment on column {table}.{new} is "{old}";'
+            rollback = f'ALTER TABLE {table} RENAME COLUMN "{new}" to "{old}";'
             alter_data[table].append(alter)
             comment_data[table].append(comment)
+            rollback_data[table].append(rollback)
 
-    return alter_data, comment_data
+    return alter_data, comment_data, rollback_data
 
 
-# TODO: исправить на DRY c предыдущей функцией get_alter_and_comment_data() в части обработки двух списков/кортежей.
+# TODO: исправить на DRY c предыдущей функцией
+#  get_alter_and_comment_data() в части обработки двух списков/кортежей.
 #  Начиная с for table in tables: и до формирования конкретных данных
 def create_xml_file(file) -> None:
     """Создание xml файла на основе текстового файла с входящими данными"""
-    alter_columns_text, comment_columns_text = get_alter_and_comment_data(file)
-    merged_dataset = merged_columns_for_tables(alter_columns_text, comment_columns_text)
+    (
+        alter_columns_text,
+        comment_columns_text,
+        rollback_columns_text,
+    ) = get_alter_and_comment_data(file)
+    merged_dataset = merged_columns_for_tables(
+        alter_columns_text, comment_columns_text, rollback_columns_text
+    )
     tables = merged_dataset.keys()
 
     i = 1
 
     for table in tables:
-        alter_rows, comment_rows = merged_dataset[table]
-        name_file = f"{datetime.now().strftime('%Y-%m-%d')}-{i}-alter_table_{table}.xml"
-        alter = '\n'.join(alter_rows)
-        comment = '\n'.join(comment_rows)
-        with open(f'{output_dir}/{name_file}', 'w', encoding='UTF-8') as f:
-            info = f'{header1} {table}\" {header2}\n{alter}\ncommit;\n\n{comment}\ncommit;\n\n{footer}'
+        alter_rows, comment_rows, rollback_rows = merged_dataset[table]
+        name_file = f"""{datetime.now().strftime('%Y-%m-%d')}-{i}-alter_table_{table}.xml"""  # noqa: E501
+        alter = "\n".join(alter_rows)
+        comment = "\n".join(comment_rows)
+        rollback = "\n".join(rollback_rows)
+        with open(f"{output_dir}/{name_file}", "w", encoding="UTF-8") as f:
+            info = f"""{header1} {table}" {header2}\n{alter}\ncommit;
+                        \n\n{comment}\ncommit;
+                        \n{rollback_start}{rollback}{rollback_end}"""
             f.write(info)
 
         i += 1
--- settings.py
+++ settings.py
@@ -1,11 +1,14 @@
-input_file = 'upload/data.txt'
-output_dir = 'download/xml'
-header1 = '''<?xml version="1.0" encoding="UTF-8"?>\n<databaseChangeLog\n
-        xmlns="http://www.liquibase.org/xml/ns/dbchangelog"\n
-        xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"\n
-        xsi:schemaLocation="http://www.liquibase.org/xml/ns/dbchangelog http://www.liquibase.org/xml/ns/dbchangelog/dbchangelog-3.9.xsd">\n
-    <changeSet id="alter table'''
+# flake8: noqa
+input_file = "upload/data.txt"
+output_dir = "download/xml"
+header1 = """<?xml version="1.0" encoding="UTF-8"?>\n<databaseChangeLog
+        xmlns="http://www.liquibase.org/xml/ns/dbchangelog"
+        xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+        xsi:schemaLocation="http://www.liquibase.org/xml/ns/dbchangelog http://www.liquibase.org/xml/ns/dbchangelog/dbchangelog-3.9.xsd">
+    <changeSet id="alter table"""
 header2 = 'author="Irkhin-VB" dbms="oracle">\n<sql>'
-#tmp = '2*NVfLB$#*1bFAWc9H1oCIW2j'
+# tmp = '2*NVfLB$#*1bFAWc9H1oCIW2j'
+rollback_start = "</sql>\n<rollback>\n\t<sql>\n"
+rollback_end = "\n</sql>\n</rollback>\n</changeSet>\n</databaseChangeLog>"
 
-footer = '</sql>\n</changeSet>\n</databaseChangeLog>'
+# footer = '</sql>\n</changeSet>\n</databaseChangeLog>'
